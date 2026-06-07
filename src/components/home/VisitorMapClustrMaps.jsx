b7a2755c94fdd147f60b46a6da2386f59f4004e5
kt4ngw <kt4ngw@163.com>
2026-06-07 16:28:34 +1000

    update VisitorMapClustrMaps.jsx

--- src/components/home/VisitorMapClustrMaps.jsx
+++ src/components/home/VisitorMapClustrMaps.jsx
@@ -11,15 +11,15 @@ export default function VisitorMapClustrMaps() {
 
     const s = document.createElement("script");
     s.type = "text/javascript";
-    s.id = "clustrmaps";
+    s.id = "mapmyvisitors";
     s.async = true;
 
     // 关键：把这里换成你拿到的“完整 src”
     // 建议用 https 开头（GitHub Pages 是 https）
-    s.src = "//cdn.clustrmaps.com/map_v2.js?cl=ffffff&w=a&t=tt&d=uf-j7wo6aXXZgccQAiYJfeOetHKxAO62aa3n2ehO2nU";
+    s.src = "https://mapmyvisitors.com/map.js?cl=ffffff&w=300&t=t&d=o0PzVxa4-2wBm3x-Oo59AOvJDjnWGQxdsZVOiehj52w&co=2d78ad&ct=ffffff&cmo=3acc3a&cmn=ff5353";
 
-    s.onload = () => console.log("[clustrmaps] loaded");
-    s.onerror = () => console.error("[clustrmaps] failed to load:", s.src);
+    s.onload = () => console.log("[mapmyvisitors] loaded");
+    s.onerror = () => console.error("[mapmyvisitors] failed to load:", s.src);
 
     el.appendChild(s);
 
